542e6cf70a815644362d3e7050673116e32707a2
jeepnroll <jean-philippe@zik-n-com.net>
2015-11-28 18:12:34 +0100

    0.0.0
    mise en place du système de fichier

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Zik'N'Com. Création de site internet pour les artistes.">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vue/css/style.css">
+    <title></title>
+</head>
+<body class="container-fluid">
+<?php
+ include('vue/page/accueil.php');
+?>
+</body>
+</html>
+
---
+++ vue/page/accueil.php
@@ -0,0 +1,6 @@
+<section class="col-lg-12" id="accueil">
+    <a href="#" class="">
+    <img src="vue/images/logo%20final.svg" alt="Logo de Zik'N'Com">
+    <p class="slogan">@robase ton style</p>
+    </a>
+</section>
