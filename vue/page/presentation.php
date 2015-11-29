89967d05fc7e84051915b97104b746f5c8c188e6
Cynthia <champyjeep@gmail.com>
2015-11-29 17:41:25 +0100

    Page Présentation fini

---
+++ vue/page/presentation.php
@@ -0,0 +1,43 @@
+<?php include('header.inc.php'); ?>
+
+    <section class="row section-presentation" id="presentation">
+        <div class="col col1">
+            <a href="#">
+                <div class="animcol"></div>
+                <div class="titreSection">
+                    <p>Artistes</p>
+                    <div class="animBall">
+                        <?php include('balle.php') ;?>
+                    </div>
+                    <div class="titreFiltre"></div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col col2">
+            <a href="#">
+                <div class="animcol"></div>
+                <div class="titreSection">
+                    <p>Associations</p>
+                    <div class="animBall">
+                        <?php include('balle.php') ;?>
+                    </div>
+                    <div class="titreFiltre"></div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col col3">
+            <a href="#">
+                <div class="animcol"></div>
+                <div class="titreSection">
+                    <p>Entreprises</p>
+                    <div class="animBall">
+                        <?php include('balle.php') ;?>
+                    </div>
+                    <div class="titreFiltre"></div>
+                </div>
+            </a>
+        </div>
+    </section>
+
