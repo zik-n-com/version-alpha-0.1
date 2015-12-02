ffe8022ef4b1a3e5a4914a8c46f6380ce18b7b4f
Cynthia <champyjeep@gmail.com>
2015-12-02 19:16:52 +0100

    Remise à zéro 1.1

--- vue/page/artistes.php
+++ vue/page/artistes.php
@@ -1,40 +1,7 @@
 <?php include('header.inc.php'); ?>
 
-
-<div class="col-md-offset-1 col-md-10 col-md-offset-1" id="page-artiste">
-
-<div class="col-lg-12">
-
-    <ul class="nav nav-justified">
-        <li class="active"><a href="#pack1" data-toggle="tab">Pack Roadies</a></li>
-        <li><a href="#pack2" data-toggle="tab">Pack Groupies</a></li>
-        <li><a href="#pack3" data-toggle="tab">Pack Goodies</a></li>
-    </ul>
-    <div class="tab-content">
-
-        <div class="tab-pane active" id="pack1">
-            <p>5 pages</p>
-            <p>Formulaire de Contact</p>
-            <p>Newsletter</p>
-            <p>Agenda</p>
-                        
-        </div>
-
-
-        <div class="tab-pane" id="pack2">
-            <p>Texte pack groupies</p>
-
-        </div>
-
-
-        <div class="tab-pane" id="pack3">
-            <p>Texte pack goodies</p>
-
-        </div>
-
-        <div class="tab-pane active" id="pack1"><p>Texte pack roadies</p></div>
-        <div class="tab-pane" id="pack2"><p>Texte pack groupies</p></div>
-        <div class="tab-pane" id="pack3"><p>Texte pack goodies</p></div>
-
-    </div>
+<div id="page-artistes" class="jumbotron">
+    <h1>artistes</h1>
+    <p></p>
 </div>
+
